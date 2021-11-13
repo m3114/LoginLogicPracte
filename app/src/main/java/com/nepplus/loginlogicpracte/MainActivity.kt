14c43205572fe2911cd2b3dd5424ff10b713ad82
김다은 <softeun92@naver.com>
2021-11-13 14:45:45 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/loginlogicpracte/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.nepplus.loginlogicpracte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener{
+            val inputId = etnId.text.toString()
+            val inputPw = edtPassWord.text.toString()
+
+            if(inputId == "admin"&& inputPw == "qwer"){
+                Toast.makeText(this,"관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+    }
+}
